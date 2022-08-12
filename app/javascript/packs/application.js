226b55449a74542dfc805a7d0a27dcc44af3c02b
Abdullah Tariq <mat57557@gmail.com>
2022-08-12 17:41:16 +0500

    Issues Resolved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,72 +13,37 @@ import "bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-// import "../../../vendor/assets/js/apexcharts.custom"
-// import "../../../vendor/assets/js/apexcharts.min"
-// import "../../../vendor/assets/js/apps"
-// import "../../../vendor/assets/js/bootstrap-datepicker.min"
-// import "../../../vendor/assets/js/bootstrap.min"
-// import "../../../vendor/assets/js/Chart.min"
-// import "../../../vendor/assets/js/config"
-// import "../../../vendor/assets/js/d3.min"
-// import "../../../vendor/assets/js/datamaps-zoomto"
-// import "../../../vendor/assets/js/datamaps.all.min"
-// import "../../../vendor/assets/js/datamaps.custom"
-// import "../../../vendor/assets/js/dataTables.bootstrap4.min"
-// import "../../../vendor/assets/js/daterangepicker"
-// import "../../../vendor/assets/js/dropzone.min"
-// import "../../../vendor/assets/js/fullcalendar.custom"
-// import "../../../vendor/assets/js/fullcalendar"
-// import "../../../vendor/assets/js/gauge.min"
-// import "../../../vendor/assets/js/jquery.dataTables.min"
-// import "../../../vendor/assets/js/jquery.mask.min"
-// import "../../../vendor/assets/js/jquery.min"
-// import "../../../vendor/assets/js/jquery.sparkline.min"
-// import "../../../vendor/assets/js/jquery.steps.min"
-// import "../../../vendor/assets/js/jquery.stickOnScroll"
-// import "../../../vendor/assets/js/jquery.timepicker"
-// import "../../../vendor/assets/js/jquery.validate.min"
-// import "../../../vendor/assets/js/moment.min"
-// import "../../../vendor/assets/js/perfect-scrollbar.min"
-// import "../../../vendor/assets/js/popper.min"
-// import "../../../vendor/assets/js/select2.min"
-// import "../../../vendor/assets/js/simplebar.min"
-// import "../../../vendor/assets/js/tinycolor-min"
-// import "../../../vendor/assets/js/topojson.min"
-// import "../../../vendor/assets/js/uppy.min"
-// import "../../../vendor/assets/js/quill.min"
-
-require('../../../vendor/assets/js/apexcharts.custom')
-require('../../../vendor/assets/js/apexcharts.min')
-require('../../../vendor/assets/js/apps')
-require('../../../vendor/assets/js/bootstrap-datepicker.min')
-require('../../../vendor/assets/js/bootstrap.min')
-require('../../../vendor/assets/js/Chart.min')
-require('../../../vendor/assets/js/config')
-require('../../../vendor/assets/js/d3.min')
-require('../../../vendor/assets/js/datamaps-zoomto')
-require('../../../vendor/assets/js/datamaps.all.min')
-require('../../../vendor/assets/js/datamaps.custom')
-require('../../../vendor/assets/js/dataTables.bootstrap4.min')
-require('../../../vendor/assets/js/daterangepicker')
-require('../../../vendor/assets/js/dropzone.min')
-require('../../../vendor/assets/js/fullcalendar.custom')
-require('../../../vendor/assets/js/fullcalendar')
-require('../../../vendor/assets/js/gauge.min')
-require('../../../vendor/assets/js/jquery.dataTables.min')
-require('../../../vendor/assets/js/jquery.mask.min')
-require('../../../vendor/assets/js/jquery.min')
-require('../../../vendor/assets/js/jquery.sparkline.min')
-require('../../../vendor/assets/js/jquery.steps.min')
-require('../../../vendor/assets/js/jquery.stickOnScroll')
-require('../../../vendor/assets/js/jquery.timepicker')
-require('../../../vendor/assets/js/jquery.validate.min')
-require('../../../vendor/assets/js/moment.min')
-require('../../../vendor/assets/js/perfect-scrollbar.min')
-require('../../../vendor/assets/js/popper.min')
-require('../../../vendor/assets/js/select2.min')
-require('../../../vendor/assets/js/simplebar.min')
-require('../../../vendor/assets/js/tinycolor-min')
-require('../../../vendor/assets/js/topojson.min')
-require('../../../vendor/assets/js/uppy.min')
-require('../../../vendor/assets/js/quill.min')
+import "../../../vendor/assets/js/apexcharts.custom"
+import "../../../vendor/assets/js/apexcharts.min"
+import "../../../vendor/assets/js/apps"
+import "../../../vendor/assets/js/bootstrap-datepicker.min"
+import "../../../vendor/assets/js/bootstrap.min"
+import "../../../vendor/assets/js/Chart.min"
+import "../../../vendor/assets/js/config"
+import "../../../vendor/assets/js/d3.min"
+import "../../../vendor/assets/js/datamaps-zoomto"
+import "../../../vendor/assets/js/datamaps.all.min"
+import "../../../vendor/assets/js/datamaps.custom"
+import "../../../vendor/assets/js/dataTables.bootstrap4.min"
+import "../../../vendor/assets/js/daterangepicker"
+import "../../../vendor/assets/js/dropzone.min"
+import "../../../vendor/assets/js/fullcalendar.custom"
+import "../../../vendor/assets/js/fullcalendar"
+import "../../../vendor/assets/js/gauge.min"
+import "../../../vendor/assets/js/jquery.dataTables.min"
+import "../../../vendor/assets/js/jquery.mask.min"
+import "../../../vendor/assets/js/jquery.min"
+import "../../../vendor/assets/js/jquery.sparkline.min"
+import "../../../vendor/assets/js/jquery.steps.min"
+import "../../../vendor/assets/js/jquery.stickOnScroll"
+import "../../../vendor/assets/js/jquery.timepicker"
+import "../../../vendor/assets/js/jquery.validate.min"
+import "../../../vendor/assets/js/moment.min"
+import "../../../vendor/assets/js/perfect-scrollbar.min"
+import "../../../vendor/assets/js/popper.min"
+import "../../../vendor/assets/js/select2.min"
+import "../../../vendor/assets/js/simplebar.min"
+import "../../../vendor/assets/js/tinycolor-min"
+import "../../../vendor/assets/js/topojson.min"
+import "../../../vendor/assets/js/uppy.min"
+import "../../../vendor/assets/js/quill.min"
