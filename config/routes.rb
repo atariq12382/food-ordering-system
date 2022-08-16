a9801dfc05773e63606abc476486d19d3a098d26
Abdullah Tariq <mat57557@gmail.com>
2022-08-16 22:40:29 +0500

    completed login and registration pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'employee/index'
   root 'managers#dashboard'
   devise_for :employees, path: 'employees'
   devise_for :admin_users, ActiveAdmin::Devise.config
